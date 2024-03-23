ca3915f6eea835908421db6f7a570de9cf4b5336
aakarshan-raj <aakarshan7raj@gmail.com>
2024-03-23 18:37:55 +0530

    Type c instruction to machine code

--- assembler.cpp
+++ assembler.cpp
@@ -1,8 +1,117 @@
 #include <iostream>
 #include <fstream>
-#include <string> 
+#include <string>
+#include <map>
 
 std::string dec_to_binary(int);
+
+struct typec_instruction
+{
+
+  std::map<std::string, std::string> dest_map = {
+      {"", "000"},
+      {"m", "001"},
+      {"d", "010"},
+      {"md", "011"},
+      {"a", "100"},
+      {"am", "101"},
+      {"ad", "110"},
+      {"amd", "111"},
+  };
+  std::map<std::string, std::string> comp_map = {
+      {"0", "0101010"},
+      {"1", "0111111"},
+      {"-1", "0111010"},
+      {"d", "0001100"},
+      {"a", "0110000"},
+      {"!d", "0001101"},
+      {"!a", "0110001"},
+      {"-d", "0001111"},
+      {"-a", "0110011"},
+      {"d+1", "0011111"},
+      {"a+1", "0110111"},
+      {"d-1", "0001110"},
+      {"a-1", "0110010"},
+      {"d+a", "0000010"},
+      {"d-a", "0010011"},
+      {"a-d", "00000111"},
+      {"d&a", "0000000"},
+      {"d|a", "0010101"},
+
+      {"", "1101010"},
+      {"", "1111111"},
+      {"", "1111010"},
+      {"", "1001100"},
+      {"m", "1110000"},
+      {"", "1001101"},
+      {"!m", "1110001"},
+      {"", "1001111"},
+      {"-m", "1110011"},
+      {"", "1011111"},
+      {"m+1", "1110111"},
+      {"", "1001110"},
+      {"m-1", "1110010"},
+      {"d+m", "1000010"},
+      {"d-m", "1010011"},
+      {"m-d", "1000111"},
+      {"d&m", "1000000"},
+      {"d|m", "1010101"},
+
+  };
+  std::map<std::string, std::string> jump_map = {
+      {"", "000"},
+      {"jgt", "001"},
+      {"jeq", "010"},
+      {"jge", "011"},
+      {"jlt", "100"},
+      {"jne", "101"},
+      {"jle", "110"},
+      {"jmp", "111"},
+  };
+  ;
+
+  std::string dest;
+  std::string comp;
+  std::string jump;
+
+  typec_instruction(std::string line)
+  {
+    std::string token;
+    for (char c : line)
+    {
+      if (c == '=')
+      {
+        dest = token;
+        token = "";
+      }
+      else if (c == ';')
+      {
+        comp = token;
+        token = "";
+      }
+      else if (c == ' ')
+      {
+        // ignore whitespaces
+      }
+      else
+      {
+        token += c;
+      }
+    }
+    if (comp == "")
+    {
+      comp = token;
+    }
+    else
+    {
+      jump = token;
+    }
+  }
+
+  std::string get_instruction(){
+    return comp_map[comp]+dest_map[dest]+jump_map[jump];
+  }
+};
 
 int main()
 {
@@ -19,24 +128,25 @@ int main()
     if (line[0] == '@')
     {
       // Type A
-      int num = std::stoi(line.substr(1,line.length()-1));
+      int num = std::stoi(line.substr(1, line.length() - 1));
       std::string type_a = dec_to_binary(num);
       output_line.append(type_a);
-      output_line.insert(0,"0");
+      output_line.insert(0, "0");
     }
     else
     {
       // Type C
+      typec_instruction ins(line);
+      output_line.append(ins.get_instruction());
+      output_line.insert(0,"111");
     }
     output_string.append(output_line);
     output_string.append("\n");
     output_line = "";
-
   }
   output << output_string;
   file.close();
   output.close();
-
 }
 
 std::string dec_to_binary(int n)
@@ -48,8 +158,9 @@ std::string dec_to_binary(int n)
     n /= 2;
   }
   int len_to_append = 15 - num.length();
-  for(int i=0;i<len_to_append;i++){
-    num.insert(0,"0");
+  for (int i = 0; i < len_to_append; i++)
+  {
+    num.insert(0, "0");
   }
   return num;
 }
